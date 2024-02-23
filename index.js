c622f4e26e3001fe34cbd5d00041ff0eb66878a5
ganga201 <gangadhar01a@gmail.com>
2024-02-23 10:26:28 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+/**
+ * Adds two numbers.
+ * @param {number} a The first number.
+ * @param {number} b The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = add;
